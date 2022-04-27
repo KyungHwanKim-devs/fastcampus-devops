fa7418ee5091d6278e0ed48d0e1bf998a269dd74
KyungHwanKim-dev <kkhdevs@gmail.com>
2022-04-27 17:33:14 +0900

    test

--- 2-terraform/04-module/main.tf
+++ 2-terraform/04-module/main.tf
@@ -6,8 +6,8 @@ module "vpc" {
   source  = "tedilabs/network/aws//modules/vpc"
   version = "0.24.0"
 
-  name                  = "fastcampus"
-  cidr_block            = "10.0.0.0/16"
+  name       = "fastcampus"
+  cidr_block = "10.0.0.0/16"
 
   internet_gateway_enabled = true
 
@@ -73,6 +73,7 @@ module "route_table__public" {
   ipv4_routes = [
     {
       cidr_block = "0.0.0.0/0"
+      # hi
       gateway_id = module.vpc.internet_gateway_id
     },
   ]
